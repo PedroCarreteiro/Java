8ee5ad30b19df366c0a56c226ca5fc2c57af6bfb
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-08-22 10:52:13 -0300

    Add files via upload

--- ConversaoDados/src/ex3.java
+++ ConversaoDados/src/ex3.java
@@ -12,23 +12,39 @@ public class ex3 {
 
         int mult = n1 * n2;
 
+//        String numero_str = String.valueOf(mult);
+//
+//        int tamanho = numero_str.length();
+//
+//        System.out.println("O resultado é: "+mult);
+//
+//        System.out.println("Quantidade de digitos no resultado: "+tamanho);
+//
+//        for(int i = 0; i <= 9; i++){
+//            int qtd = 0;
+//            for (int j = 0; j < tamanho; j++){
+//                if (numero_str.substring(j, j + 1).equals(String.valueOf(i))){
+//                    qtd++;
+//
+//                }
+//
+//            }
+//        }
+
         String numero_str = String.valueOf(mult);
 
-        int tamanho = numero_str.length();
+        String numeros = "0123456789";
 
-        System.out.println("O resultado é: "+mult);
-
-        System.out.println("Quantidade de digitos no resultado: "+tamanho);
-
-        for(int i = 0; i <= 9; i++){
-            int qtd = 0;
-            for (int j = 0; j < tamanho; j++){
-                if (numero_str.substring(j, j + 1).equals(String.valueOf(i))){
-                    qtd++;
-                    if(qtd > 0){
-                        System.out.printf("O digito %d aparece %d vez(es) no resultado\n",i,qtd);
-                    }
+        int cont = 0;
+        for(int i = 0; i < numeros.length(); i++){
+            cont = 0;
+            for(int j = 0; j < numero_str.length(); j++){
+                if(numeros.charAt(i)==numero_str.charAt(j)){
+                    cont++;
                 }
+            }
+            if(cont != 0){
+                System.out.printf("O digito %c aparece %d vez(es) no resultado\n",numeros.charAt(i),cont);
             }
         }
     }
