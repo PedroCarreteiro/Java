8354e2b4d0f5d84d29da0da29c9060b440dcd2f7
--global <--global>
2025-09-12 08:02:15 -0300

    Correção ex1 trycatch

--- TryCatch/src/ex1.java
+++ TryCatch/src/ex1.java
@@ -6,28 +6,34 @@ public class ex1 {
 
         boolean entrada = true;
         int qtd_notas = 0;
-        int media = 0;
-        int notas = 0;
+        double media = 0;
+        double notas = 0;
 
 
         while (entrada){
             try {
-                System.out.println("Digite a nota");
-                int nota = Integer.parseInt(scanner.nextLine());
-                if(nota > 0){
+                System.out.println("Digite a "+(qtd_notas + 1)+"° nota");
+                double nota = Double.parseDouble(scanner.nextLine());
+                if(nota >= 0){
                     notas+=nota;
                     qtd_notas++;
                 }
                 else{
                     entrada = false;
                 }
-            } catch (Exception e){
+            } catch (NumberFormatException e){
                 System.out.println("Número inválido!\nDigite novamente\n");
             }
         }
 
-        media = notas/qtd_notas;
+        try{
+            media = notas/qtd_notas;
+            System.out.println("A média é: "+media);
+        } catch (ArithmeticException e){
+            System.out.println("Não foi inserido nenhuma nota!");
+        }
 
-        System.out.println("A média é: "+media);
+
+
     }
 }
